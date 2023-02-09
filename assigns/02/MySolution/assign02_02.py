9a7e470612a63a2322c355b02ab4e37adb8727f8
mqitao <tmengqi@bu.edu>
2023-02-09 10:52:43 -0500

    assignment 02

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -10,3 +10,25 @@ print("[import ./../assign02.py] is done!")
 # mylist_quicksort (see list_quicksort in assign02.sml)
 #
 ####################################################
+from assign02_01 import mylist_append
+def qpart(xs, p0):
+	if xs.ctag == 0:
+		return (mylist_nil(), mylist_nil())
+	elif xs.cons1 <= p0:
+		(ys, zs) = qpart(xs.cons2, p0)
+		return (mylist_cons(xs.cons1, ys), zs)
+	else: 
+		(ys, zs) = qpart(xs.cons2, p0)
+		return (ys, mylist_cons(xs.cons1, zs))
+
+
+def mylist_quicksort(xs):
+	if xs.ctag == 0:
+		return mylist_nil()
+	else:
+		(ys, zs) = qpart(xs.cons2, xs.cons1)
+		ys = mylist_quicksort(ys)
+		zs = mylist_quicksort(zs)
+		return mylist_append(ys, mylist_cons(xs.cons1, zs))
+    
+	
--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -10,3 +10,34 @@ print("[import ./../assign02.py] is done!")
 # mylist_mergesort (see list_mergesort in assign02.sml)
 #
 ####################################################
+def split(xs):
+	if xs.ctag == 0:
+		return (mylist_nil(), mylist_nil())
+	else:
+		if xs.cons2.ctag == 0:
+			return (mylist_cons(xs.cons1,mylist_nil()), mylist_nil())
+		else:
+			(ys, zs) = split(xs.cons2.cons2)
+			return(mylist_cons(xs.cons1, ys), mylist_cons(xs.cons2.cons1, zs))
+
+def merge(ys, zs):
+	if ys.ctag == 0:
+		return zs
+	else:
+		if zs.ctag == 0:
+			return ys
+		else:
+			if ys.cons1 <= zs.cons1:
+				return mylist_cons(ys.cons1, merge(ys.cons2, zs))
+			else:
+				return mylist_cons(zs.cons1, merge(zs.cons2, ys))
+
+def mylist_mergesort(xs):
+	if xs.ctag == 0:
+		return mylist_nil()
+	else:
+		if xs.cons2.ctag == 0:
+			return mylist_cons(xs.cons1, mylist_nil())
+		else:
+			(ys, zs) = split(xs.cons2.cons2)
+			return merge(mylist_mergesort(mylist_cons(xs.cons1, ys)), mylist_mergesort(mylist_cons(xs.cons2.cons1, zs)))
